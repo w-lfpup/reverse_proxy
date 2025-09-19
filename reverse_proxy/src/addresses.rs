d6a519396c0f9ed7a0855ba27d9dd6c0ef525824
Taylor Vann <brian.t.vann@gmail.com>
2025-09-19 16:33:40 -0700

    use authority not host

--- reverse_proxy/src/addresses.rs
+++ reverse_proxy/src/addresses.rs
@@ -4,13 +4,11 @@ use std::collections::HashMap;
 use crate::config::Config;
 
 pub fn create_address_map(config: &Config) -> Result<HashMap<String, (Uri, bool)>, String> {
-    // get host incoming
     let mut hashmap = HashMap::<String, (Uri, bool)>::new();
     if let Err(e) = add_addresses_to_map(&mut hashmap, &config.addresses, false) {
         return Err(e);
     };
 
-    // get (host and port, is dangerous) outgoing
     if let Some(self_signed_addresses) = &config.dangerous_self_signed_addresses {
         if let Err(e) = add_addresses_to_map(&mut hashmap, &self_signed_addresses, true) {
             return Err(e);
@@ -25,14 +23,13 @@ fn add_addresses_to_map(
     addresses: &Vec<(String, String)>,
     is_dangerous: bool,
 ) -> Result<(), String> {
-    // get uri for target, get host and port
     for (source_str, target_str) in addresses {
         let source_uri = match Uri::try_from(source_str) {
             Ok(uri) => uri,
             Err(e) => return Err(e.to_string()),
         };
 
-        let source_host = match source_uri.host() {
+        let source_host = match source_uri.authority() {
             Some(h) => h,
             _ => return Err("could not parse host from source uri".to_string()),
         };
